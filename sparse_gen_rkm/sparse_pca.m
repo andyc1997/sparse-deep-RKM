b16c3822fcf92ea0196585b23419fa8a49e77469
AndyC_WC <45092498+andyc1997@users.noreply.github.com>
2022-03-07 18:18:20 +0100

    Add files via upload

--- sparse_gen_rkm/sparse_pca.m
+++ sparse_gen_rkm/sparse_pca.m
@@ -1,13 +1,22 @@
-function [V, D, K] =  sparse_pca(X, ft_map, params, s)
-    % Compute top s pairs of sparse eigenvectors (V) and eigenvalues (D)
-    % dim(X) = number of obs * number of features
+function [V, D, K] =  sparse_pca(X, ft_map, params, s, no_sparse, on_stm)
+    % X: data matrix
+    % ft_map: neural network object
+    % params: parameters for kernel function {'name', value}
+    % s: number of pc
+    % no_sparse: 1 for sparse PCA; 0 for classical PCA
+    % on_stm: 1 for Stiefel manifold; 0 for Euclidean space
     
+    % Compute top s pairs of sparse eigenvectors (V) and eigenvalues (D)
+    % dim(X) = num of obs * num of features
+    % dim(Xf) = num of obs * new num of features
+   
     state = 'explicit';
     N = size(X, 1);
-    K = zeros(N);
     
-    assert(N > 0, 'number of sample must be positive');
-    assert(s <= N, 'number of the principal components cannot be greater than the sample size');
+    assert(N > 0, 'N must be positive');
+    assert(s <= N, 's cannot be greater than N');
+    assert(no_sparse == 1 || no_sparse == 0, 'no_sparse must be either 0 or 1')
+    assert(on_stm == 1 || on_stm == 0, 'on_stm must be either 0 or 1')
     
     % check explicit or implicit feature map
     if ft_map == []
@@ -21,7 +30,7 @@ function [V, D, K] =  sparse_pca(X, ft_map, params, s)
     % explicit feature map
     elseif strcmp(state, 'explicit') == 1
         Xf = predict(ft_map, X);
-        K = Xf'*Xf;
+        K = Xf*Xf';
         
     % invalid input
     else
@@ -33,6 +42,24 @@ function [V, D, K] =  sparse_pca(X, ft_map, params, s)
     P = (eye(N) - ones(N)./N);
     K = P*K*P;
     
-    % sparse PCA
+    % classical PCA
+    [V, D] = eigs(K, s);
+    D = diag(D);
+    if no_sparse == 1
+        return
+    end
     
+    if on_stm == 0
+        % sparse PCA - Gpower method l0 norm, Euclidean space
+        gamma = 0.1*ones(1, s);  
+        V = GPower(K, gamma.^2, s, 'l0', 0);
+        D = diag(V'*K*V);
+        
+    elseif on_stm == 1
+        % sparse PCA - Gpower method l0 norm, Stiefel manifold
+        gamma = 0.1*ones(1, s);
+        mu = (1:s).^(-1); 
+        V = GPower(K, gamma.^2, s, 'l0', 1, mu);
+        D = diag(V'*K*V);
+    end
 end
--- sparse_gen_rkm/train.m
+++ sparse_gen_rkm/train.m
@@ -4,7 +4,23 @@ cd 'C:\Users\user\Documents\Statistics\SPCA-DRKM-2021JUL01\work\new-function\sha
 img = loadMNISTImages('..\train-images-idx3-ubyte\train-images.idx3-ubyte');
 
 
+%% Sample data without replacement
+N_subset = 5000;
+rng(80116);
+Xtr = datasample(img, N_subset, 2, 'Replace', false);
 
+
+%% Train
+s = 10;
+N = N_subset;
+K = build_kernel(Xtr', {'rbf_func', 0.5});
+P = (eye(N) - ones(N)./N);
+K = P*K*P;
+
+[V, D] = eigs(K, s);
+%%
+gamma = 0.1*ones(1, s);  
+Vs = GPower(K, gamma.^2, s, 'l0', 0);
 
 %%
 % src: https://github.com/davidstutz/matlab-mnist-two-layer-perceptron/blob/master/loadMNISTImages.m
