f70995891e6fcf975238bce672e2f9b2d8412320
AndyC_WC <45092498+andyc1997@users.noreply.github.com>
2022-03-12 13:19:59 +0100

    Add files via upload

--- sparse_gen_rkm/multiview_skpca_rkm.m
+++ sparse_gen_rkm/multiview_skpca_rkm.m
@@ -1,16 +1,36 @@
-function [V, D, K] =  multiview_skpca_rkm(X, Y, ft_map_x, ft_map_y, params, s, no_sparse, on_stm)
+function [V, D, K] =  multiview_skpca_rkm(X, Y, ft_map_x, ft_map_y, params, s, method)
     % ****************************************************************
     % X: data matrix, view1
     % Y: data matrix, view2
-    % ft_map: neural network object
-    % params: parameters for kernel function {'name', value}
+    % ft_map_x / ft_map_y: neural network object
+    % params: parameters {'name', value}
     % s: number of pc
-    % no_sparse: 0 for sparse PCA; 1 for classical PCA
-    % on_stm: 0 for Euclidean space; 1 for Stiefel manifold
-    
+    % method: PCA, GPowerl0, GPowerl0_block, MMl0, TPower
+    %
     % Compute top s pairs of sparse eigenvectors (V) and eigenvalues (D)
     % dim(X) = num of obs * num of features
     % dim(Xf) = num of obs * new num of features
+    %
+    % Syntax
+    %
+    % PCA:
+    % params = {'rbf_func', double, 'eta1', double, 'eta2', double}
+    %
+    % GPowerl0:
+    % params = {'rbf_func', double, 'eta1', double, 'eta2', double, 'gamma', double or array of size 1 by s}
+    %
+    % GPowerl0_block:
+    % params = {'rbf_func', double, 'eta1', double, 'eta2', double, 'gamma', double or array of size 1 by s,
+    % 'mu', [] or array of size 1 by s}
+    %
+    % MMl0:
+    % params = {'rbf_func', double, 'eta1', double, 'eta2', double, 'rhos', double or array of size 1 by s}
+    %
+    % TPower:
+    % params = {'rbf_func', double, 'eta1', double, 'eta2', double, 'rhos', double or array of size 1 by s,
+    % rf: double}
+    %
+    % 
     % ****************************************************************
     
     state = 'explicit';
@@ -18,16 +38,13 @@ function [V, D, K] =  multiview_skpca_rkm(X, Y, ft_map_x, ft_map_y, params, s, n
     Ny = size(Y, 1);
     
     assert((Nx > 0 && Ny > 0) && (Nx == Ny), 'Nx and Ny must be positive, and equal');
-    assert(s <= Nx, 's cannot be greater than Nx + Ny');
-    assert(no_sparse == 1 || no_sparse == 0, 'no_sparse must be either 0 or 1')
-    assert(on_stm == 1 || on_stm == 0, 'on_stm must be either 0 or 1')
-    assert(strcmp(params{3}, 'eta1') && strcmp(params{5}, 'eta2'), 'eta1 and eta2 must be provided')
-    
-    if no_sparse == 0
-        assert(strcmp(params{7}, 'gamma'), 'sparsity factor must be provided')
-    end
     
     N = Nx;
+    assert(s <= N, 's cannot be greater than N');
+    
+    assert(strcmp(params{3}, 'eta1') && strcmp(params{5}, 'eta2'), 'eta1 and eta2 must be provided');
+    assert((params{4} > 0) && (params{6} > 0), 'eta1 and eta2 must be positive scalars');
+    
     
     % check explicit or implicit feature map
     if isempty(ft_map_x) && isempty(ft_map_y) == 1
@@ -59,24 +76,85 @@ function [V, D, K] =  multiview_skpca_rkm(X, Y, ft_map_x, ft_map_y, params, s, n
     % classical PCA
     [V, D] = eigs(K, s);
     D = diag(D);
-    if no_sparse == 1
-        return
-    end
     
-    Xg = V*sqrt(abs(diag(D)))*V';
-    if on_stm == 0
-        % sparse PCA - Gpower method l0 norm, Euclidean space
-        gamma = params{8} / max(sum(K.^2, 1));
-        gamma = gamma*ones(1, s);  
-        V = GPower(Xg, gamma.^2, s, 'l0', 0);
-        D = diag(V'*K*V);
+    switch method
+        case 'PCA' % kernal PCA
+            return
         
-    elseif on_stm == 1
-        % sparse PCA - Gpower method l0 norm, Stiefel manifold
-        gamma = params{8} / max(sum(K.^2, 1));
-        gamma = gamma*ones(1, s);
-        mu = (1:s).^(-1); 
-        V = GPower(Xg, gamma.^2, s, 'l0', 1, mu);
-        D = diag(V'*K*V);
+        case 'GPowerl0' % sparse kernel PCA - Gpower method l0 norm
+            % guess data matrix
+            Xg = V*sqrt(abs(diag(D)))*V';        
+            
+            % check if gamma is a scalar, expand as an array
+            assert(strcmp(params{7}, 'gamma'), 'sparsity factor gamma must be provided');
+            gamma = params{8};
+            [~, ngam] = size(gamma);
+            if ngam == 1
+                gamma = gamma*ones(1, s);
+            end
+            
+            % run sparse pca
+            V = GPower(Xg, gamma, s, 'l0', 0);
+            D = diag(V'*K*V);
+        
+        case 'GPowerl0_block' % sparse PCA - Gpower method l0 norm, Stiefel manifold
+            % guess data matrix
+            Xg = V*sqrt(abs(diag(D)))*V';        
+            
+            % check if gamma is a scalar, expand as an array
+            assert(strcmp(params{7}, 'gamma'), 'sparsity factor gamma must be provided');
+            gamma = params{8};
+            [~, ngam] = size(gamma);
+            if ngam == 1
+                gamma = gamma*ones(1, s);
+            end
+            
+            % check if mu is passed as an argument
+            assert(strcmp(params{9}, 'mu'), 'parameter mu must be provided');
+            if isempty(params{10}) == 1
+                mu = (1:s).^(-1);
+            else
+                mu = params{10};
+                [~, nmu] = size(mu);
+                assert(nmu == s, 'mu must be of length s');
+            end
+            
+            % run sparse pca
+            V = GPower(Xg, gamma, s, 'l0', 1, mu);
+            D = diag(V'*K*V);
+       
+        case 'MMl0' % MM algorithm with l0 norm
+            % check if rhos are passed as an argument
+            assert(strcmp(params{7}, 'rhos'), 'penalty coefficient rhos must be provided');
+            rhos = params{8};
+            [~, nrhos] = size(rhos);
+            if nrhos == 1
+                rhos = rhos*ones(1, s);
+            end
+            
+            % run sparse pca
+            V = mm_sparse_eigen(K, [], s, rhos);
+            D = diag(V'*K*V);
+        
+        case 'TPower' % TPower method
+            % check if rhos are passed as an argument
+            assert(strcmp(params{7}, 'rhos'), 'cardinality rhos must be provided');
+            rhos = params{8};
+            [~, nrhos] = size(rhos);
+            if nrhos == 1
+                rhos = rhos*ones(1, s);
+            end
+            
+            % check if rf is provided
+            assert(strcmp(params{9}, 'rf'), 'reducing factor must be provided');
+            rf = params{10};
+            
+            % run sparse pca
+            V = TPower(K, s, rhos, rf);
+            D = diag(V'*K*V);
+            
+        otherwise
+            catch_msg = 'invalid method for sparse PCA';
+            error(catch_msg);
     end
 end
